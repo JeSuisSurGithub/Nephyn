f1fe7056beb0681d6b5803be53ee6c9aca87da94
JutiliseGit <jutilisegougeul@gmail.com>
2024-10-02 10:27:01 +0200

    ddrp simplification

--- src/main.rs
+++ src/main.rs
@@ -23,9 +23,9 @@ fn nephynika(input_path: &str, delta_path: &str, downres_path: &str, ds_factor: 
         .decode()
         .expect("Failed to decode image").to_rgb8();
 
-    let yuv_img = rgb_to_yuv(&img);
+    // let yuv_img = rgb_to_yuv(&img);
 
-    let (d_img, ds_img) = delta_down_res_predictor(&yuv_img, ds_factor);
+    let (d_img, ds_img) = delta_down_res_predictor(&img, ds_factor);
 
     d_img
         .save(Path::new(delta_path))
@@ -52,9 +52,9 @@ fn denephynika(delta_path: &str, downres_path: &str, output_path: &str, ds_facto
 
     let yuv_img = dedelta_down_res_predictor(&d_img, &ds_img, ds_factor);
 
-    let img = yuv_to_rgb(&yuv_img);
+    // let img = yuv_to_rgb(&yuv_img);
 
-    img
+    yuv_img
         .save(Path::new(output_path))
         .expect("Failed to save output image");
 
@@ -63,25 +63,27 @@ fn denephynika(delta_path: &str, downres_path: &str, output_path: &str, ds_facto
 
 fn main()
 {
-    {
-        let start = Instant::now();
+    nephynika("experiments/sirin_020.bmp", "experiments/delta2.bmp", "experiments/dr2.bmp", 4);
+    denephynika("experiments/delta2.bmp", "experiments/dr2.bmp", "experiments/restore2.bmp", 4);
+    // {
+    //     let start = Instant::now();
 
-        let infile = File::open("experiments/sirin_delta.bmp").unwrap();
-        let mut outfile = File::create("experiments/sirin_delta.lzw").unwrap();
-        lzw_encode(&mut infile.take(u64::MAX), &mut outfile).unwrap();
+    //     let infile = File::open("experiments/sirin_delta.bmp").unwrap();
+    //     let mut outfile = File::create("experiments/sirin_delta.lzw").unwrap();
+    //     lzw_encode(&mut infile.take(u64::MAX), &mut outfile).unwrap();
 
-        let duration = start.elapsed();
-        println!("Time taken: {:?}", duration);
-    }
-    {
-        let start = Instant::now();
+    //     let duration = start.elapsed();
+    //     println!("Time taken: {:?}", duration);
+    // }
+    // {
+    //     let start = Instant::now();
 
-        let infile = File::open("experiments/sirin_delta.lzw").unwrap();
-        let mut outfile = File::create("experiments/sirin_delta_restored.bmp").unwrap();
+    //     let infile = File::open("experiments/sirin_delta.lzw").unwrap();
+    //     let mut outfile = File::create("experiments/sirin_delta_restored.bmp").unwrap();
 
-        lzw_decode(&mut infile.take(u64::MAX), &mut outfile).unwrap();
+    //     lzw_decode(&mut infile.take(u64::MAX), &mut outfile).unwrap();
 
-        let duration = start.elapsed();
-        println!("Time taken: {:?}", duration);
-    }
+    //     let duration = start.elapsed();
+    //     println!("Time taken: {:?}", duration);
+    // }
 }
